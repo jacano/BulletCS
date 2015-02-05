1db9e372422bb043dc083531c7c8695d1e53f463
Juan Antonio Cano <jacano@plainconcepts.com>
2015-02-05 15:45:08 +0100

    - Few more changes

--- bullet-2.83-alpha/swigBullet.bat
+++ bullet-2.83-alpha/swigBullet.bat
@@ -3,6 +3,16 @@
 set "CURRENTDIR=%~dp0"
 set PATH=%CURRENTDIR%..\swig-master\;%PATH%
 
-swig -c++ -csharp -includeall -ignoremissing -namespace BulletSharp -outdir BulletSharp -o wrapper/bullet_wrap.cpp BulletSwig.i
+rmdir /s /q ..\BulletCS.Shared\SWIG
+timeout 1
+mkdir ..\BulletCS.Shared\SWIG
+
+rmdir /s /q wrapper
+timeout 1
+mkdir wrapper
+
+timeout 1
+
+swig -c++ -csharp -includeall -ignoremissing -namespace BulletSharp -outdir ../BulletCS.Shared/SWIG -o wrapper/bullet_wrap.cpp BulletSwig.i
 
 pause
